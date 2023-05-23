20f0bfd84579336b796e31070307bf774ace26f8
Job Vonk <sticcpistt@icloud.com>
2023-05-23 23:06:32 +0200

    Added ι-rules for Nat, Atom, Π and Σ eliminators

--- core.ts
+++ core.ts
@@ -5,95 +5,168 @@ type Symbol = string;
 type Renaming = I.Map<Symbol, Symbol>;
 
 export abstract class Core {
-  public abstract eval(gamma: Context<V.Value>): V.Value;
-  public abstract alpha_equiv(
-      other: Core, context?: { left: Renaming, right: Renaming }): void;
+  public abstract eval(_gamma: V.Rho): V.Value {
+      throw new Error(`Cannot evaluate ${this}`);
+  }
+
+  public abstract alpha_equiv(other: Core, context?: { left: Renaming, right: Renaming }): void;
 }
 
-export class Var implements Core {
-  public constructor(public name: Symbol) { }
-  public eval(_gamma: Context<V.Value>) { return new V.Nat(); } // TODO
+export class Var extends Core {
+    public constructor(public name: Symbol) { super(); }
+    public override eval(gamma: V.Rho): V.Value {
+        if (gamma.has(this.name)) {
+            return gamma.get(this.name) as V.Value;
+        } else {
+            throw new Error(`Could not find variable ${this.name}`);
+        }
+    }
 }
 
-export class Nat implements Core {
-  public eval(_gamma: Context<V.Value>) { return new V.Nat(); }
+export class Nat extends Core {
+    public override eval(_gamma: V.Rho) {
+        return new V.Nat();
+    }
 }
 
-export class Atom implements Core {
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); }
+export class Atom extends Core {
+    public override eval(_gamma: V.Rho): V.Value {
+        return new V.Atom();
+    }
 }
 
-export class Tick implements Core {
-  public constructor(public name: Symbol) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+export class Tick extends Core {
+  public constructor(public name: Symbol) { super(); }
+  public override eval(_gamma: V.Rho): V.Value {
+      return new V.Tick(this.name);
+  }
 }
 
 export class Sigma implements Core {
-  public constructor(public name: Symbol, public value: Core, public body: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+  public constructor(public name: Symbol, public value: Core, public body: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_value = this.value.eval(gamma);
+      const clos_body = new V.Closure(gamma, this.body);
+      return new V.Sigma(this.name, eval_value, clos_body);
+  }
 }
 
-export class Cons implements Core {
-  public constructor(public left: Core, public right: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+export class Cons extends Core {
+  public constructor(public left: Core, public right: Core) { super(); }
+  public override eval(context: V.Rho): V.Value {
+      return new V.Cons(this.left.eval(context), this.right.eval(context));
+  }
 }
 
-export class Car implements Core {
-  public constructor(public pair: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+export class Car extends Core {
+  public constructor(public pair: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_pair = this.pair.eval(gamma) as V.Cons;
+      return eval_pair.fst;
+  }
 }
 
-export class Cdr implements Core {
-  public constructor(public pair: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+export class Cdr extends Core {
+  public constructor(public pair: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_pair = this.pair.eval(gamma) as V.Cons;
+      return eval_pair.snd;
+  }
 }
 
-export class Pi implements Core {
-  public constructor(public name: Symbol, public value: Core, public body: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+export class Pi extends Core {
+  public constructor(public name: Symbol, public value: Core, public body: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const clos_body = new V.Closure(gamma, this.body);
+      return new V.Pi(this.name, this.value.eval(gamma), clos_body);
+  }
 }
 
-export class Lambda implements Core {
-  public constructor(public name: Symbol, public body: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+export class Lambda extends Core {
+  public constructor(public name: Symbol, public body: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const clos_body = new V.Closure(gamma, this.body);
+      return new V.Lambda(this.name, clos_body);
+  }
 }
 
-export class Appl implements Core {
-  public constructor(public func: Core, public arg: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Atom(); } // TODO
+export class Appl extends Core {
+  public constructor(public func: Core, public arg: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_func = this.func.eval(gamma);
+      const eval_arg = this.arg.eval(gamma);
+      return V.apply_many(eval_func, eval_arg);
+  }
 }
 
-export class Zero implements Core {
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Zero(); } // TODO
+export class Zero extends Core {
+  public override eval(_gamma: V.Rho): V.Value {
+      return new V.Zero();
+  }
 }
 
-export class Add1 implements Core {
-  public constructor(public num: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Zero(); } // TODO
+export class Add1 extends Core {
+  public constructor(public num: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_n = this.num.eval(gamma);
+      return new V.Add1(eval_n);
+  }
 }
 
-export class WhichNat implements Core {
+export class WhichNat extends Core {
   public constructor(public target: Core, public base_type: V.Value,
-                     public base_expr: Core, public add1: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Zero(); } // TODO
+                     public base_expr: Core, public add1: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_target = this.target.eval(gamma);
+      if (eval_target instanceof V.Zero) {
+          return this.base_expr.eval(gamma);
+      } else {
+          return this.add1.eval(gamma);
+      }
+  }
 }
 
-export class IterNat implements Core {
+export class IterNat extends Core {
   public constructor(public target: Core, public base_type: V.Value,
-                     public base_expr: Core, public add1: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Zero(); } // TODO
+                     public base_expr: Core, public add1: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_target = this.target.eval(gamma);
+      return IterNat.do(eval_target, this.base_expr.eval(gamma), this.add1.eval(gamma));
+  }
+
+  public static do(n: V.Zero | V.Add1, base: V.Value, step: V.Value): V.Value {
+      if (n instanceof V.Zero) {
+          return base;
+      } else {
+          return V.apply_many(step, IterNat.do(n.n, base, step)); 
+      }
+  }
 }
 
-export class RecNat implements Core {
+export class RecNat extends Core {
   public constructor(public target: Core, public base_type: V.Value,
-                     public base: Core, public add1: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Zero(); } // TODO
+                     public base: Core, public add1: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_target = this.target.eval(gamma);
+      return RecNat.do(eval_target, this.base.eval(gamma), this.add1.eval(gamma));
+  }
+
+  public static do(n: V.Add1 | V.Zero, base: V.Value, step: V.Value): V.Value {
+      if (n instanceof V.Zero) {
+          return base;
+      } else {
+          return V.apply_many(step, n.n, RecNat.do(n.n, base, step));
+      }
+  }
 }
 
-export class IndNat implements Core {
+export class IndNat extends Core {
   public constructor(public target: Core, public motive: Core,
-                     public base_expr: Core, public add1: Core) { }
-  public eval(_gamma: Context<V.Value>): V.Value { return new V.Zero(); } // TODO
+                     public base_expr: Core, public add1: Core) { super(); }
+  public override eval(gamma: V.Rho): V.Value {
+      const eval_target = this.target.eval(gamma);
+      return RecNat.do(eval_target, this.base_expr.eval(gamma), this.add1.eval(gamma));
+  }
 }
 
 export class List implements Core {
